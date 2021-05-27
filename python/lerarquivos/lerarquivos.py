d5654014a66c9093f47ed565708e76574168b23c
Vitor Fréz <vih.frez@gmail.com>
2021-05-27 01:04:45 -0300

    Update lerarquivos.py

--- python/lerarquivos/lerarquivos.py
+++ python/lerarquivos/lerarquivos.py
@@ -1,35 +1,37 @@
-import os.path
 import time
 import os
+
 loop = True
+nome_arquivo = "suport.txt"
+
 while loop:
  os.system('cls')
- time.sleep(3)
- if(os.path.isfile('suport.txt')):
-    arquivoread = open('suport.txt', 'r')
-    for arquivoprincipalnameread in arquivoread:
-        if(os.path.isfile(arquivoprincipalnameread)):
-            arquivoreadname = open(arquivoprincipalnameread, 'r')
-            for linha in arquivoreadname:
-                print(linha)
-            arquivoreadname.close()
-            loop = False
-        else:
-            print("O arquivo não existe porfavor digite as informações do arquivo principal:")
-            definirname = input('escreva o nome do arquivo')
-            creat = open(definirname, 'w')
-            suportedit = open('suport.txt', 'w')
-            suportedit.write(definirname)
-            definirvalor = input('escreva oque tera no arquivo')
-            creat.write(definirvalor)
-            os.system('cls')
-            print("Porfavor espere Criando o arquivo...")
-            time.sleep(5)
-            suportedit.close()
-            creat.close()
+ #time.sleep(3)
+ 
+ path_arquivo = os.path.abspath(nome_arquivo)
+ 
+ if(os.path.isfile(path_arquivo)):
+    arquivo_read = open(path_arquivo, 'r')
+    
+    print("Conteúdo do arquivo: ")
+    for linha in arquivo_read:
+        print(linha)
+        
+    arquivo_read.close()
+    
+    print("\n\nFinalizando aplicação...")
+    loop = False
+    
  else:
-    suportestart = open('suport.txt', 'w')
-    suportestart.write('arquivo=null')
-    suportestart.close()
-    print('Um erro foi achado tente dar start novamente')
-    os.system('msg * Um erro foi achado tente dar start novamente')
+    print("O arquivo padrão não existe! \nVamos criar um novo!")
+    nome_arquivo = input('Digite o nome do novo arquivo: ')
+    criar_arquivo = open(nome_arquivo, 'w')
+
+    conteudo_arquivo = input(f"Digite o conteúdo do arquivo {nome_arquivo}: ")
+    criar_arquivo.write(conteudo_arquivo)
+    
+    os.system('cls')
+    
+    print("Criando arquivo, aguarde...")
+    time.sleep(2)
+    criar_arquivo.close()
