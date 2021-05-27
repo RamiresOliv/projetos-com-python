e35b7697d03b1e0c1df48d6aa752852171d28542
gabrielramires <gabrielramiresdeoliveira1@gmail.com>
2021-05-26 22:06:00 -0300

    lerarquivos update

--- python/lerarquivos/lerarquivos.py
+++ python/lerarquivos/lerarquivos.py
@@ -1,7 +1,11 @@
 import os.path
+import time
 import os
-os.system('cls')
-if(os.path.isfile('suport.txt')):
+loop = True
+while loop:
+ os.system('cls')
+ time.sleep(3)
+ if(os.path.isfile('suport.txt')):
     arquivoread = open('suport.txt', 'r')
     for arquivoprincipalnameread in arquivoread:
         if(os.path.isfile(arquivoprincipalnameread)):
@@ -9,17 +13,21 @@ if(os.path.isfile('suport.txt')):
             for linha in arquivoreadname:
                 print(linha)
             arquivoreadname.close()
+            loop = False
         else:
+            print("O arquivo não existe porfavor digite as informações do arquivo principal:")
             definirname = input('escreva o nome do arquivo')
             creat = open(definirname, 'w')
             suportedit = open('suport.txt', 'w')
             suportedit.write(definirname)
             definirvalor = input('escreva oque tera no arquivo')
-            print("O arquivo não existe")
             creat.write(definirvalor)
-            creat.close()
+            os.system('cls')
+            print("Porfavor espere Criando o arquivo...")
+            time.sleep(5)
             suportedit.close()
-else:
+            creat.close()
+ else:
     suportestart = open('suport.txt', 'w')
     suportestart.write('arquivo=null')
     suportestart.close()
